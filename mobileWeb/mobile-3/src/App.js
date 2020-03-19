80cce272508f6b2e60f0e221454ead9f138c7d2f
YanQiMing <1024744235@qq.com>
2020-03-19 23:00:11 +0800

    mobile第三次实验

---
+++ mobileWeb/mobile-3/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+
+
+export default class App extends Component{
+  render(){
+    return (
+
+      <HashRouter>
+        <Switch>
+          <Route path='/' component={}/>
+        </Switch>
+      </HashRouter>
+
+    );
+  }
+}
+
+
+
+
