5f86d3cd0575a00392c845d6a7c6c9032e6ab67d
yanqiming5 <yanqiming5@jd.com>
2021-02-22 13:54:46 +0800

    commit

---
+++ PWA/离线缓存/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'cache-cat'
+
+self.addEventListener('install',function(event){
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './cat.png'
+            ])
+        })
+    )
+})
+
+self.addEventListener('activate',function(event){
+    console.log(event)
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            console.log(cacheNames)
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName != CACHE_NAME
+                }).map(function(cacheName){
+                    return caches.delete(cacheName)
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener('fetch',function(event){
+    console.log(event)
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if(response){
+                return response
+            }
+            const fetchRequest = event.request.clone()
+            return fetch(fetchRequest).then(function(response){
+                const responseToCache = response.clone()
+                caches.open(CACHE_NAME).then(function(cache){
+                    cache.put(event.request,responseToCache)
+                })
+                return response
+            })
+        })
+    )
+})
